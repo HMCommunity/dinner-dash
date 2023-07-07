e8204b9c1842a0a1ceb8e40e5d5b5c47d1c7ad4f
Ademayowa <ademayowaadeniyi@gmail.com>
2023-07-07 15:24:42 +0100

    created dummy navbar links

---
+++ constants/index.tsx
@@ -0,0 +1,17 @@
+export const navLinks = [
+  {
+    id: 'faqs',
+    title: 'FAQS',
+    page: '#',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+    page: '#',
+  },
+  {
+    id: 'about',
+    title: 'About',
+    page: '#',
+  },
+];
