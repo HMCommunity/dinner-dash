b7d674882769d023921fe3ff008ce6a5e7b947cd
Ademayowa <ademayowaadeniyi@gmail.com>
2023-07-07 14:21:37 +0100

    project setup

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'DinnerDash',
+  description: 'Order for food|Afordable Meals',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+type Props = {};
+
+export default function Hero({}: Props) {
+  return (
+    <div className='grid place-items-center mt-10'>
+      <h1 className='text-3xl'>Dinner Dash Landing Page</h1>
+    </div>
+  );
+}
